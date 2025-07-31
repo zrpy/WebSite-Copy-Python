f206e73357f5f25a7a699f0bc50ddba9a0ddeca8
zrpy <91083604+zrpy@users.noreply.github.com>
2025-07-31 18:14:59 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+DESCRIPTION = 'Websiteを簡単にコピーしてくれるパッケージです'
+
+setup(
+    name="dispyplus",
+    version=VERSION,
+    author="meow",
+    description=DESCRIPTION,
+    long_description=DESCRIPTION
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "beautifulsoup4>=4.13.4",
+        "requests>=2.32.4",
+    ],
+    keywords=['python', 'website', 'clone', 'copy', 'cloning'],
+    url="https://github.com/zrpy/WebSite-Copy-Python/",
+    project_urls={
+        "Source Code": "https://github.com/meowkawaiijp/Discord.py-Plus",
+    },
+)
